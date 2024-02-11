9d774328d4c7b7b61bb91bc67d0f920760d4f95f
chrisIyer <hellohotbox420@gmail.com>
2024-02-11 13:21:53 -0500

    card components

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,4 @@
-import CardImage from "./Image";
+import Image from "./Image";
 import { Card } from "react-bootstrap";
 
 export default function CardBody({blocks}) {
@@ -6,7 +6,7 @@ export default function CardBody({blocks}) {
      return (
           <Card className="card-body">
                <h5 className="card-title">{blocks.alt}</h5>
-               <CardImage/>
+               <Image/>
           </Card>
      );
 }
