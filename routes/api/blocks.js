53e22875436ccff3798e9bea42898ebe70902671
chrisIyer <hellohotbox420@gmail.com>
2024-02-11 11:41:46 -0500

    more files for the bCKEND. running into an error. I think I've got other node apps still open.

---
+++ routes/api/blocks.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+const blockCtrl = require('../../controllers/api/blocks')
+
+
+router.delete('/:id', blockCtrl.destroyBlock, blockCtrl.respondWithBlock)
+router.put('/:id', blockCtrl.updateBlock, blockCtrl.respondWithBlock)
+router.post('/', blockCtrl.createBlock, blockCtrl.respondWithBlock)
+router.get('/:id', blockCtrl.getBlocks, blockCtrl.respondWithBlock)
+router.get('/', blockCtrl.getBlocks, blockCtrl.respondWithBlocks)
+module.exports = router
