509cbb43cab5c7868b95a4b8d9474f2630f03caf
chrisIyer <hellohotbox420@gmail.com>
2024-02-11 19:45:28 -0500

    foubd source of error.

--- src/components/SortableItem.js
+++ src/components/SortableItem.js
@@ -1,9 +1,38 @@
+// import { useSortable } from "@dnd-kit/sortable";
+// import {CSS} from "@dnd-kit/utilities";
+// import Card from 'react-bootstrap/Card';
+
+// export function SortableItem(props) {
+//     // props.id
+//     // JavaScript
+
+//     const {
+//         attributes,
+//         listeners,
+//         setNodeRef,
+//         transform,
+//         transition
+//     } = useSortable({id: props.id});
+
+//     const style = {
+//         transform: CSS.Transform.toString(transform),
+//         transition
+//     }
+
+//     return (
+//         <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+//             <Card className="m-3">{props.id}</Card>
+          
+//         </div>
+//     )
+// }
+//starting here follow this format
 import { useSortable } from "@dnd-kit/sortable";
-import {CSS} from "@dnd-kit/utilities";
+import { CSS } from "@dnd-kit/utilities";
 import Card from 'react-bootstrap/Card';
 
-export function SortableItem(props) {
-    // props.id
+export function SortableItem({ block }) {
+    // block.id
     // JavaScript
 
     const {
@@ -12,7 +41,7 @@ export function SortableItem(props) {
         setNodeRef,
         transform,
         transition
-    } = useSortable({id: props.id});
+    } = useSortable({ id: block.id });
 
     const style = {
         transform: CSS.Transform.toString(transform),
@@ -21,8 +50,21 @@ export function SortableItem(props) {
 
     return (
         <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-            <Card className="m-3">{props.id}</Card>
-          
+            <Card 
+            // body className="m-3"
+                // style={{
+                //     width: '15rem',
+                //     display: "grid",
+                //     alignText: "left",
+                //     gap: "1rem"
+                // }}
+                >
+                <Card.Body>
+                    <Card.Title>{block.alt}</Card.Title>
+                    <Card.Subtitle>{block.alt}</Card.Subtitle>
+                    <img src={block.img} alt={block.img}>{block.img}</img>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
