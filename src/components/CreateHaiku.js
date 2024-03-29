57f4d515043f0ced75559603bd0182f6a0626e1b
chrisIyer <hellohotbox420@gmail.com>
2024-03-29 17:42:32 -0400

    need to bring form above haikus.

--- src/components/CreateHaiku.js
+++ src/components/CreateHaiku.js
@@ -1,5 +1,4 @@
 import { Form } from 'react-bootstrap';
-import ColorPicker from './ColorPicker';
 
 
 export default function CreateHaiku({
@@ -11,26 +10,26 @@ export default function CreateHaiku({
     <>
       <h2>Create A Haiku</h2>
       <div className='container'>
-        <Form
+        <Form style={{width: '40%', position: 'sticky', float: 'right'}}
           className='form'
           onSubmit={(e) => {
             e.preventDefault()
             createHaiku()
           }}
         >
-          <div>
-            <Form.Select aria-label="Default select example" value={haiku.author} onChange={handleChange} name="author">
+          <div >
+            <Form.Select style=  {{backgroundColor: 'rgba( 224,175,155,0.3)'}} aria-label="Default select example" value={haiku.author} onChange={handleChange} name="author">
               <option>Author</option>
               <option value="chris">Chris</option>
               <option value="laura">Laura</option>
               <option value="leah">Leah</option>
               <option value="paul">Paul</option>
             </Form.Select>
-            <Form.Control type='text' value={haiku.one} name='one' onChange={handleChange} placeholder='Line 1' />
-            <Form.Control type='text' value={haiku.two} name='two' onChange={handleChange} placeholder='Line 2' />
-            <Form.Control type='text' value={haiku.three} name='three' onChange={handleChange} placeholder='Line 3' />
-            <Form.Control type='text' value={haiku.title} name='title' onChange={handleChange} placeholder='Optional title' />
-            <Form.Control style={{backgroundColor:haiku.color}}type="color" id="exampleColorInput" value={haiku.color} name="color"  title="Choose your color" onChange={handleChange}/>
+            <Form.Control style=  {{backgroundColor: 'rgba( 224,175,155,0.1)'}} type='text' value={haiku.one} name='one' onChange={handleChange} placeholder='Line 1' />
+            <Form.Control style=  {{backgroundColor: 'rgba( 224,175,155,0.1)'}}type='text' value={haiku.two} name='two' onChange={handleChange} placeholder='Line 2' />
+            <Form.Control style=  {{backgroundColor: 'rgba( 224,175,155,0.1)'}} type='text' value={haiku.three} name='three' onChange={handleChange} placeholder='Line 3' />
+            <Form.Control style=  {{backgroundColor: 'rgba( 224,175,155,0.1)'}} type='text' value={haiku.title} name='title' onChange={handleChange} placeholder='Optional title' />
+            <Form.Control style={{backgroundColor:haiku.color}}type="color" id="exampleColorInput" value={haiku.color} name="color" defaultValue= "#ff0000" title="Choose your color" onChange={handleChange}/>
           </div>
          
           <input style={{color: 'red'}}className='button' type='submit' value='Create Haiku' />
--- src/components/Haiku.js
+++ src/components/Haiku.js
@@ -25,7 +25,7 @@ export default function Haiku({
   const inputRefE = useRef(null)
   return (
     < >
-      <Card className={setClass(haiku,styles)} style={{ width: '20%', height: '10%', padding: '1%', margin: '1%', textAlign: 'left'}}>
+      <Card className={setClass(haiku,styles)} style={{ width: '20%', height: '10%', padding: '1%', margin: '1%', textAlign: 'left', boxShadow: '12px 12px 12px 11px haiku.color'}}>
         <Card.Title onClick={() => setShowE(!showE)}> T {haiku.title}</Card.Title>
         <input
           ref={inputRefE}
